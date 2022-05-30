9a62aca54f52e5cc32a09f5846d1d64c87815804
Nayaralimar <nayara.l.r@hotmail.com>
2022-05-29 22:11:08 -0300

    feat: update pub/sub adicionando funcionalidade do botão dark theme

---
+++ src/scripts.js
@@ -0,0 +1,42 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+    botaoConteudo: getStyle(html, "--botao-conteudo"),
+    segundoConteudo: getStyle(html, "--segundo-conteudo"),
+}
+
+const darkMode = {
+    bg: "#333333",
+    bgPanel: "#434343",
+    colorHeadings: "#3664FF",
+    colorText: "#B5B5B5",
+    botaoConteudo: "#4F4F4F",
+    segundoConteudo: "#696969",
+
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
