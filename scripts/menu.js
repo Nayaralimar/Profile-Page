655ad7ca5b4e657a336e4570dd5c5a3de8f6310e
Nayaralimar <nayara.l.r@hotmail.com>
2022-07-21 16:31:45 -0300

    fix: fix mobile menu scroll

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,10 +2,48 @@ let show = true;
 
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
+const menuMobile = menuSection.querySelector(".menu-mobile")
+const menuMobileTwo = menuSection.querySelector(".menu-mobile-two")
+const menuMobileThree = menuSection.querySelector(".menu-mobile-three")
+const menuMobileFour = menuSection.querySelector(".menu-mobile-four")
+
+
 
 menuToggle.addEventListener("click", () => {
 
     document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
+
+menuMobile.addEventListener("click", () => {
+
+    document.body.style.overflow = "scroll"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
+
+menuMobileTwo.addEventListener("click", () => {
+
+    document.body.style.overflow = "scroll"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
+
+menuMobileThree.addEventListener("click", () => {
+
+    document.body.style.overflow = "scroll"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
+
+menuMobileFour.addEventListener("click", () => {
+
+    document.body.style.overflow = "scroll"
 
     menuSection.classList.toggle("on", show)
     show = !show;
